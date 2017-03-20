8550945885848550a414e1f09dd0919b37632ff1
w205 <w205@ip-172-31-4-29.ec2.internal>
2017-03-20 20:07:45 +0000

    minor changes

--- Twittercredentials.py
+++ Twittercredentials.py
@@ -1,16 +1,16 @@
 import tweepy
 
-consumer_key = "YourConsumerkey";
+consumer_key = "hSRTIgIOcRlducBVkUJJr8AIV";
 #eg: consumer_key = "YisfFjiodKtojtUvW4MSEcPm";
 
 
-consumer_secret = "YourConsumerSecret";
+consumer_secret = "bQohiy0n3gUrAUfXtDwxVTE5LZg7NcXzCFGTpyDWRIiyLnAoma";
 #eg: consumer_secret = "YisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPm";
 
-access_token = "YourAcessToken";
+access_token = "843594662606602240-j3qA2UWgPEtoWMQhfWuwHQMDpleRq01";
 #eg: access_token = "YisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPm";
 
-access_token_secret = "YourTokenSecret";
+access_token_secret = "lMJMGL7zXJEvrEigy92EwbzmJ3dwbi66acJ1UfPZ0F9FQ";
 #eg: access_token_secret = "YisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPm";
 
 
